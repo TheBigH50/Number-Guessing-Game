48b97c6765914e35b338b790649ea48df3d7604b
TheBigH50 <HFangmeier@outlook.com>
2022-08-23 13:32:53 -0500

    base of game set up

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("Hello World")
+
+
+let rand = 7
+let guess 
+do {
+guess = parseInt(prompt("Guess A Number"));
+if (guess < rand) {
+    alert(`${guess} is too low`);
+} else if (guess > rand) {
+   alert(`${guess} is too high`);
+} else {
+   alert(`${guess} IS CORRECT!`);
+};
+} while (rand != guess);
