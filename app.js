aef2fe313ea45c3c95c15a1fd5b0661e17c0bdbd
TheBigH50 <HFangmeier@outlook.com>
2022-08-23 14:13:20 -0500

    move along

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 console.log("Hello World")
 
 
-let rand = 7
+let gameStart = parseInt(prompt("Choose Difficulty (1-Easy|2-Hard|3-Overkill"));
+switch (true) {
+    case go == 1: 
+        var rand = Math.floor(Math.random() * 10) +1;
+        break;
+        case go == 2:
+            var rand = Math.floor(Math.random() * 100) +1;
+            break;
+            case go == 3:
+            var rand = Math.floor(Math.random() * 1000) +1;
+            break;
+            default:
+                alert(`Sorry ${go} Is Not A Valid Difficulty`);
+};
 let guess 
 do {
 guess = parseInt(prompt("Guess A Number"));
@@ -11,5 +24,6 @@ if (guess < rand) {
    alert(`${guess} is too high`);
 } else {
    alert(`${guess} IS CORRECT!`);
+   console.log(`WAY TO GO YOU GUESSED THE NUMBER ${guess} YOU ARE SO SMART!`)
 };
 } while (rand != guess);
